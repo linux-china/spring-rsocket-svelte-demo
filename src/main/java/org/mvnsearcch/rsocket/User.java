55063e49417123ff2f210479c743e99b7e284d48
linux_china <libing.chen@gmail.com>
2020-10-19 01:06:03 -0700

    User

---
+++ src/main/java/org/mvnsearcch/rsocket/User.java
@@ -0,0 +1,35 @@
+package org.mvnsearcch.rsocket;
+
+/**
+ * User
+ *
+ * @author linux_china
+ */
+public class User {
+    private Integer id;
+    private String nick;
+
+    public User() {
+    }
+
+    public User(Integer id, String nick) {
+        this.id = id;
+        this.nick = nick;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+}
