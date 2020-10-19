74eb159d35c05c5aeb750b45b638d0b574244dfa
linux_china <libing.chen@gmail.com>
2020-10-19 01:05:54 -0700

    RSocketServiceApp

---
+++ src/main/java/org/mvnsearcch/rsocket/RSocketServiceApp.java
@@ -0,0 +1,13 @@
+package org.mvnsearcch.rsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RSocketServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RSocketServiceApp.class, args);
+	}
+
+}
